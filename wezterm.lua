c8338c083b4cc9e1441153cc05e32e6e520790e5
chris-jantzen <cjantzen22@gmail.com>
2024-02-29 18:31:39 -0500

    add buttons to title bar

--- wezterm.lua
+++ wezterm.lua
@@ -15,7 +15,7 @@ config.font = wezterm.font_with_fallback({
 })
 
 config.window_background_opacity = 0.9
-config.window_decorations = "RESIZE"
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.window_close_confirmation = "AlwaysPrompt"
 config.scrollback_lines = 3000
 config.default_workspace = "main"
